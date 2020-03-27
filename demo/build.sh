99ab714e50d391f8751c8f6e5c26ca012c3e3dcd
Martin Michaelis <code@mgjm.de>
2020-03-27 20:13:19 +0100

    Fr 27 Mär 2020 20:13:19 CET

--- demo/build.sh
+++ demo/build.sh
@@ -1,3 +1,22 @@
 #!/bin/sh -e
 
-echo 'Hello World!'
+section() {
+	echo "##[section]$@"
+}
+
+run() {
+	echo "##[command]$@"
+	echo "##[group]Execute $1"
+	local code
+	"$@"; code=$?
+	echo "##[endgroup]"
+	return $code
+}
+
+section demo
+run echo 123
+
+section demo-end
+run false
+
+section demo-end2
