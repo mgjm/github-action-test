fd023659942777f29b38efd641567e023ae44c06
Martin Michaelis <code@mgjm.de>
2020-03-27 23:07:40 +0100

    Fr 27 Mär 2020 23:07:40 CET

--- demo/build.sh
+++ demo/build.sh
@@ -19,7 +19,7 @@ run echo 123
 section demo-end
 run true
 
-echo "::set-output name=ARTIFACT_NAME::demo-0.1.0.test.txt"
+echo "::set-env name=ARTIFACT_NAME::demo-0.1.0.test.txt"
 run sh -c 'date > artifact.txt'
 
 section demo-end2
